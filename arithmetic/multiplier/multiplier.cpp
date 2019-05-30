6c8885a7c1f964ff911bbf796546e00fea1494d1
Stephen Henry <stephenry@gmail.com>
2019-05-30 21:19:20 +0100

    Multipler answer ported to GTest. Failing at present.

--- arithmetic/multiplier/multiplier.cpp
+++ arithmetic/multiplier/multiplier.cpp
@@ -25,127 +25,184 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //========================================================================== //
 
-#include <libtb2.hpp>
-#include <deque>
-#include "vobj/Vmultiplier.h"
+#define TB_ENABLE_TRACING
 
-typedef Vmultiplier uut_t;
+#include <systemc>
+#include <scv.h>
+#include <gtest/gtest.h>
+#include <queue>
+#include <sstream>
+#include "vobj/Vmultiplier.h"
+#ifdef TB_ENABLE_TRACING
+#  include "verilated_vcd_sc.h"
+#endif
+
+using word_type = uint32_t;
+using result_type = vluint64_t;
 
 #define PORTS(__func)                           \
-  __func(a, uint32_t)                           \
-  __func(b, uint32_t)                           \
+  __func(a, word_type)                          \
+  __func(b, word_type)                          \
   __func(pass, bool)                            \
-  __func(y, vluint64_t)                         \
+  __func(y, result_type)                        \
   __func(y_vld_r, bool)                         \
   __func(busy_r, bool)
 
-class MultiplierTb : libtb2::Top<uut_t> {
-
-  struct Expect {
-    Expect(uint32_t a, uint32_t b)
-        : a(a), b(b)
-    {}
-    uint32_t a, b;
-  };
-  
- public:
-  SC_HAS_PROCESS(MultiplierTb);
-  MultiplierTb(sc_core::sc_module_name mn = "t")
-      : uut_("uut")
-      , clk_("clk")
-      , rst_("rst")
-#define __construct_ports(__name, __type)       \
-      , __name ## _ (#__name )
-      PORTS(__construct_ports)
-#undef __construct_ports
-  {
-    //
-    resetter_.clk(clk_);
-    resetter_.rst(rst_);
-    //
-    sampler_.clk(clk_);
-    //
-    wd_.clk(clk_);
-    //
-    uut_.clk(clk_);
-    uut_.rst(rst_);
-#define __bind_ports(__name, __type)            \
-    uut_.__name(__name ## _);
-    PORTS(__bind_ports)
-#undef __bind_ports
-
-    SC_THREAD(t_stimulus);
-    SC_METHOD(m_checker);
-    sensitive << sampler_.sample();
-    dont_initialize();
-    register_uut(uut_);
-    vcd_on();
+static struct TOP {
+  TOP() {
+    v.rst(rst);
+    v.clk(clk);
+#define __bind_signals(__name, __type)          \
+    v.__name(__name);
+    PORTS(__bind_signals)
+#undef __bind_signals
+    start_tracing();
   }
- private:
-  void t_stimulus() {
-    struct stimulus_constraint : scv_constraint_base {
-      scv_smart_ptr<uint32_t> v;
-      SCV_CONSTRAINT_CTOR(stimulus_constraint) {
-        SCV_CONSTRAINT(v() < 1024);
-      }
-    } a("a_constrained"), b("b_constrained");
-    
-    resetter_.wait_reset_done();
-    while (true) {
-      LIBTB2_ASSERT(!busy_r_);
-
-      a.next();
-      b.next();
-
-      const uint32_t actual_a = *a.v;
-      const uint32_t actual_b = *b.v;
-      
-      a_ = actual_a;
-      b_ = actual_b;
-      pass_ = true;
-      expected_.push_back(Expect(actual_a, actual_b));
-      wait_cycles();
-      pass_ = false;
-      wait_cycles(5);
-    }
+  ~TOP() {
+    stop_tracing();
   }
-  void m_checker() {
-    if (y_vld_r_) {
-      const Expect e = expected_.front(); expected_.pop_front();
-      const vluint64_t actual = y_;
-
-      const vluint64_t expected = (
-          static_cast<vluint64_t>(e.a) * static_cast<vluint64_t>(e.b));
-      if (actual != expected) {
-        LOGGER(ERROR) << "Validating: a=" << e.a << " b=" << e.b
-                      << "\tMismatch detected:"
-                      << " expected= " << expected
-                      << " actual= " << actual
-                      << "\n";
-      } else {
-        LOGGER(INFO) << "Validating: a=" << e.a << " b=" << e.b
-                     << "\tValidated! y=" << expected << "\n";
-      }
-    }
+  void set_idle() {
+    pass = false;
+    a = word_type{};
+    b = word_type{};
   }
-  void wait_cycles(std::size_t cycles = 1) {
-    while (cycles--)
-      wait(clk_.posedge_event());
+  void t_apply_reset() {
+    rst = true;
+    t_await_cycles(2);
+    rst = false;
+    t_await_cycles(2);
   }
-  std::deque<Expect> expected_;
-  sc_core::sc_clock clk_;
-  sc_core::sc_signal<bool> rst_;
+  void t_await_cycles(std::size_t n = 1) {
+    while (n--)
+      wait(clk.negedge_event());
+  }
+  Vmultiplier v{"multiplier"};
+  ::sc_core::sc_signal<bool> rst{"rst"};
+  ::sc_core::sc_clock clk{"clk"};
 #define __declare_signal(__name, __type)        \
-  sc_core::sc_signal<__type> __name##_;
+  ::sc_core::sc_signal<__type> __name{#__name};
   PORTS(__declare_signal)
 #undef __declare_signal
-  libtb2::Resetter resetter_;
-  libtb2::Sampler sampler_;
-  libtb2::SimWatchDogCycles wd_;
-  uut_t uut_;
+ private:
+  void start_tracing() {
+#ifdef TB_ENABLE_TRACING
+    Verilated::traceEverOn(true);
+    vcd_ = std::make_unique<VerilatedVcdSc>();
+    v.trace(vcd_.get(), 99);
+    vcd_->open("sim.vcd");
+#endif
+  }
+  void stop_tracing() {
+#ifdef TB_ENABLE_TRACING
+    vcd_->close();
+#endif
+  }
+#ifdef TB_ENABLE_TRACING
+  std::unique_ptr<VerilatedVcdSc> vcd_;
+#endif
+} TOP;
+
+struct Expect {
+  Expect(word_type a, word_type b, result_type y)
+      : a_(a), b_(b), y_(y) {}
+  word_type a() const { return a_; }
+  word_type b() const { return b_; }
+  result_type y() const { return y_; }
+ private:
+  word_type a_, b_;
+  result_type y_;
 };
 
+std::ostream & operator<<(std::ostream & os, const Expect & ex) {
+  return os << "'{a=" << ex.a() << ", b=" << ex.b() << ", y=" << ex.y() << "}";
+}
+
+static class TB : public ::sc_core::sc_module {
+ public:
+  SC_HAS_PROCESS(TB);
+  TB() :
+      ::sc_core::sc_module{::sc_core::sc_module_name{"MultiplierTest"}} {
+    SC_THREAD(t_stimulus);
+    
+    SC_METHOD(m_checker);
+    sensitive << TOP.clk.negedge_event();
+    dont_initialize();
+  }
+  void add_stimulus(word_type a, word_type b, result_type y) {
+    stimulus_.push(Expect{a,b,y});
+  }
+  void run_until_exhausted() {
+    e_start_tb_.notify(1, SC_NS);
+    while (!stimulus_.empty())
+      ::sc_core::sc_start(10, SC_US);
+  }
+
+ private:
+  void t_stimulus() {
+    while (true) {
+      wait(e_start_tb_);
+      t_main_loop();
+    }
+  }
+  void t_main_loop() {
+    TOP.set_idle();
+    TOP.t_apply_reset();
+    while (!stimulus_.empty()) {
+      const Expect & ex{stimulus_.front()};
+
+      TOP.pass = true;
+      TOP.a = ex.a();
+      TOP.b = ex.b();
+      TOP.t_await_cycles(1);
+
+      expected_.push(ex);
+      stimulus_.pop();
+    }
+    TOP.set_idle();
+    wait (100, SC_NS);
+  }
+  void m_checker() {
+    if (TOP.y_vld_r) {
+      EXPECT_FALSE(expected_.empty());
+
+      const Expect & ex{expected_.front()};
+      EXPECT_EQ(TOP.y, ex.y());
+      if (TOP.y != ex.y()) {
+        std::ostringstream os;
+        os << "Stimulus mismatch: " << ex;
+        const std::string s{os.str()};
+        SC_REPORT_ERROR("/stephenry", s.c_str());
+      }
+      expected_.pop();
+    }
+  }
+  std::queue<Expect> stimulus_, expected_;
+  ::sc_core::sc_event e_start_tb_;
+} TB;
+
+TEST(MultiplierTest, Basic) {
+  struct stimulus_constraint : scv_constraint_base {
+    scv_smart_ptr<uint32_t> v;
+    SCV_CONSTRAINT_CTOR(stimulus_constraint) {
+      SCV_CONSTRAINT(v() < 1024);
+    }
+  } a_ptr("a_constrained"), b_ptr("b_constrained");
+
+  for (std::size_t i = 0; i < 1024; i++) {
+    const uint32_t a = *a_ptr.v;
+    const uint32_t b = *b_ptr.v;
+
+    TB.add_stimulus(a, b, (a * b));
+
+    a_ptr.next();
+    b_ptr.next();
+  }
+  TB.run_until_exhausted();
+}
+
 int sc_main(int argc, char ** argv) {
-  MultiplierTb tb;
-  return libtb2::Sim::start(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  ::sc_core::sc_report_handler::set_actions(
+       "/stephenry", SC_LOG | SC_DISPLAY | SC_STOP);
+  return RUN_ALL_TESTS();
 }
