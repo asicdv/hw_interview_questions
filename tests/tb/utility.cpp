17c7c3eac83c9e0938206aa5a8c11b95d1de4078
Stephen Henry <stephenry@gmail.com>
2019-06-03 15:36:36 +0100

    Addition of popcount utility and associated unit test.

---
+++ tests/tb/utility.cpp
@@ -0,0 +1,51 @@
+//========================================================================== //
+// Copyright (c) 2019, Stephen Henry
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//========================================================================== //
+
+#include "libtb.hpp"
+#include <unordered_map>
+
+TEST(Utility, PopulationCount) {
+  EXPECT_EQ(tb::pop_count(0), 0);
+  for (std::size_t n = 0; n < 31; n++)
+    EXPECT_EQ(tb::pop_count(1 << n), 1);
+
+  const std::unordered_map<std::size_t, std::size_t> m{
+    { 0, 0},
+    { 0x00000001, 1 },
+    { 0x00000003, 2 },
+    { 0x00000007, 3 },
+    { 0x0000000F, 4 },
+    { 0xAAAAAAAA, 16 },
+  };
+  for (auto & it : m)
+    EXPECT_EQ(tb::pop_count(it.first), it.second);
+}
+
+int sc_main(int argc, char ** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
