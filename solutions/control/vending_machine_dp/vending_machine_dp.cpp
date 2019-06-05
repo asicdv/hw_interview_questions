8750fd2c5a3e08a7938ba78dba88f85e30b5f7aa
Stephen Henry <stephenry@gmail.com>
2019-06-05 18:01:09 +0100

    Port vending_machine_dp solution.

--- solutions/control/vending_machine_dp/vending_machine_dp.cpp
+++ solutions/control/vending_machine_dp/vending_machine_dp.cpp
@@ -25,7 +25,9 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //========================================================================== //
 
-#include <libtb2.hpp>
+#include "libtb/libtb.hpp"
+#include "libtb/verilator.hpp"
+#include <gtest/gtest.h>
 #include "vobj/Vvending_machine_dp.h"
 
 #define PORTS(__func)                           \
@@ -39,130 +41,202 @@
   __func(change_done, bool)                     \
   __func(change_emit_dime_r, bool)
 
-typedef Vvending_machine_dp uut_t;
-
-struct VendingMachineDpTb : libtb2::Top<VendingMachineDpTb> {
-  enum CoinType { NICKEL, DIME, QUARTER };
-
-  SC_HAS_PROCESS(VendingMachineDpTb);
-  VendingMachineDpTb(sc_core::sc_module_name mn = "t")
-      : uut_("uut")
-#define __construct_signals(__name, __type)     \
-        , __name##_(#__name)
-        PORTS(__construct_signals)
-#undef __construct_signals
-  {
-    //
-    resetter_.clk(clk_);
-    resetter_.rst(rst_);
-    //
-    wd_.clk(clk_);
-    sampler_.clk(clk_);
-
-    SC_THREAD(t_stimulus);
-    SC_THREAD(t_emit_serve_done);
-    SC_THREAD(t_emit_change_done);
-    SC_THREAD(t_dispense);
-
-    uut_.clk(clk_);
-    uut_.rst(rst_);
+struct TOP : tb::Top {
+  TOP() {
+    v.clk(clk);
+    v.rst(rst);
 #define __bind_signals(__name, __type)          \
-    uut_.__name(__name##_);
+    v.__name(__name);
     PORTS(__bind_signals)
 #undef __bind_signals
+    start_tracing();
   }
+  ~TOP() {
+    stop_tracing();
+  }
+  void set_idle() {
+    client_nickel = false;
+    client_dime = false;
+    client_quarter = false;
+    client_dispense = false;
+    serve_done = false;
+    change_done = false;
+  }
+  // Setters
+  void set_client_nickel(bool x = true) { client_nickel = x; }
+  void set_client_dime(bool x = true) { client_dime = x; }
+  void set_client_quarter(bool x = true) { client_quarter = x; }
+  void set_client_dispense(bool x = true) { client_dispense = x; }
+  void set_serve_done(bool x = true) { serve_done = x; }
+  void set_change_done(bool x = true) { change_done = x; }
+  // Getters
+  bool get_client_enough_r() const { return client_enough_r; }
+  bool get_emit_irn_bru_r() const { return serve_emit_irn_bru_r; }
+  bool get_change_emit_dime_r() const { return change_emit_dime_r; }
+  void t_await_cycles(std::size_t n = 1) {
+    while (n--)
+      wait(clk.negedge_event());
+  }
+  void t_apply_reset() {
+    set_idle();
+    rst = true;
+    t_await_cycles(2);
+    rst = false;
+    t_await_cycles(2);
+  }
+  Vvending_machine_dp v{"vending_machine_dp"};
+  ::sc_core::sc_clock clk{"clk"};
+  ::sc_core::sc_signal<bool> rst{"rst"};
+#define __declare_signal(__name, __type)        \
+  ::sc_core::sc_signal<__type> __name{#__name};
+  PORTS(__declare_signal)
+#undef __declare_signal
  private:
-  void t_stimulus() {
-    LOGGER(INFO) << "Stimulus starts...\n";
-    test_0();
-    LOGGER(INFO) << "Stimulus ends\n";
-
-    wait();
+  void start_tracing() {
+#ifdef OPT_ENABLE_TRACE
+    Verilated::traceEverOn(true);
+    vcd_ = std::make_unique<VerilatedVcdSc>();
+    v.trace(vcd_.get(), 99);
+    vcd_->open("sim.vcd");
+#endif
   }
-
-  void test_0() {
-    LOGGER(INFO) << "Test 0";
-    issue_coin(QUARTER);
-    issue_coin(QUARTER);
-    LOGGER(INFO) << "\tPass\n";
-
-    wait_cycles(100);
+  void stop_tracing() {
+#ifdef OPT_ENABLE_TRACE
+    vcd_->close();
+#endif
   }
-
-  void issue_idle() {
-    client_nickel_ = false;
-    client_dime_ = false;
-    client_quarter_ = false;
-  }
-
-  void issue_coin(CoinType c) {
-    issue_idle();
-    client_nickel_ = (c == NICKEL);
-    client_dime_ = (c == DIME);
-    client_quarter_ = (c == QUARTER);
-    wait_cycles();
-    issue_idle();
-  }
-
-  void reset_change_count() { change_count_ = 0; }
-
-  void t_dispense() {
-
-    const int DLY = 2;
-    while (1) {
-      client_dispense_ = false;
-      wait(client_enough_r_.posedge_event());
-      wait_cycles(DLY);
-      client_dispense_ = true;
-      wait_cycles();
-      client_dispense_ = false;
-    }
-  }
-
-  void t_emit_change_done() {
-    const int DLY = 4;
-    while (1) {
-      change_done_ = false;
-      wait(change_emit_dime_r_.posedge_event());
-      wait_cycles(DLY);
-      change_count_++;
-      change_done_ = true;
-      wait_cycles();
-    }
-  }
-
-  void t_emit_serve_done() {
-    const int DLY = 3;
-    while (1) {
-      serve_done_ = false;
-      wait(serve_emit_irn_bru_r_.posedge_event());
-      wait_cycles(DLY);
-      serve_done_ = true;
-      wait_cycles();
-    }
-  }
-
-  void wait_cycles(std::size_t cycles = 1) {
-    while (cycles-- > 0)
-      wait(clk_.posedge_event());
-  }
-
-  std::size_t change_count_;
-  libtb2::Resetter resetter_;
-  libtb2::SimWatchDogCycles wd_;
-  libtb2::Sampler sampler_;
-  sc_core::sc_clock clk_;
-  sc_core::sc_signal<bool> rst_;
-#define __declare_signals(__name, __type)       \
-  sc_core::sc_signal<__type> __name##_;
-  PORTS(__declare_signals)
-#undef __declare_signals
-  uut_t uut_;
+#ifdef OPT_ENABLE_TRACE
+  std::unique_ptr<VerilatedVcdSc> vcd_;
+#endif
 };
-SC_MODULE_EXPORT(VendingMachineDpTb);
 
-int sc_main (int argc, char **argv) {
-  VendingMachineDpTb tb;
-  return libtb2::Sim::start(argc, argv);
+namespace {
+
+struct VendingMachineModel {
+  VendingMachineModel() { reset(); }
+  
+  void set_nickel(bool x = true) { if (x) sum_ += 5; }
+  void set_dime(bool x = true) { if (x) sum_ += 10; }
+  void set_quarter(bool x = true) { if (x) sum_ += 25; }
+
+  std::size_t sum() const { return sum_; }
+  bool vend() const { return sum_ >= 40; }
+  bool change() const { return sum_ > 40; }
+
+  void reset() { sum_ = 0; }
+  void step() {}
+ private:
+  std::size_t sum_{0};
+};
+
+TOP top;
+tb::TaskRunner TaskRunner;
+
+} // namespace
+
+TEST(VendingMachineFsmTest, Basic) {
+  struct VendingMachineFsmTask : ::tb::Task {
+    VendingMachineFsmTask(TOP & top, std::size_t n = 1024)
+        : top_(top), n_(n) {}
+    void execute() override {
+      scenario_1();
+      //      scenario_2();
+      finish();
+    }
+   private:
+    enum class Coin { Nickel, Dime, Quarter, Invalid };
+    
+    void scenario_1() {
+      // Add dimes until machine vends
+      tb::Random::Bag<Coin> bg_coin;
+      bg_coin.add(Coin::Nickel, 80);
+      bg_coin.add(Coin::Dime, 20);
+      bg_coin.add(Coin::Quarter, 5);
+      bg_coin.add(Coin::Invalid, 20);
+      bg_coin.finalize();
+
+      VendingMachineModel mdl{};
+      top_.t_apply_reset();
+
+      while (!mdl.vend())
+        issue_coin(bg_coin(), mdl);
+      
+      ASSERT_TRUE(top_.get_client_enough_r());
+      ASSERT_FALSE(top_.get_emit_irn_bru_r());
+      ASSERT_FALSE(top_.get_change_emit_dime_r());
+
+      top_.set_serve_done();
+      top_.t_await_cycles();
+      top_.set_idle();
+    }
+    void scenario_2() {
+      // Add dimes and nickel to cause machine to vend with change.
+      VendingMachineModel mdl{};
+      top_.t_apply_reset();
+
+      for (std::size_t i = 0; i < 7; i++)
+        issue_coin(Coin::Nickel, mdl);
+
+      ASSERT_FALSE(top_.get_client_enough_r());
+      ASSERT_FALSE(top_.get_emit_irn_bru_r());
+      ASSERT_FALSE(top_.get_change_emit_dime_r());
+
+      issue_coin(Coin::Dime, mdl);
+
+      ASSERT_TRUE(top_.get_client_enough_r());
+      ASSERT_TRUE(top_.get_change_emit_dime_r());
+
+      top_.set_serve_done();
+      top_.t_await_cycles();
+      top_.set_idle();
+
+      top_.set_change_done();
+      top_.t_await_cycles();
+      top_.set_idle();
+
+      ASSERT_FALSE(top_.get_client_enough_r());
+      ASSERT_FALSE(top_.get_change_emit_dime_r());
+    }
+    void issue_coin(Coin coin, VendingMachineModel & mdl) {
+      switch (coin) {
+        case Coin::Nickel:
+          top_.set_client_nickel();
+          mdl.set_nickel();
+          break;
+        case Coin::Dime:
+          top_.set_client_dime();
+          mdl.set_dime();
+          break;
+        case Coin::Quarter:
+          top_.set_client_quarter();
+          mdl.set_quarter();
+          break;
+        case Coin::Invalid:
+          // NOP
+          break;
+      }
+      top_.t_await_cycles();
+      top_.set_idle();
+    }
+    void get_change() {
+      if (top_.get_change_emit_dime_r()) {
+        for (std::size_t i = 0; i < 2; i++)
+          top_.t_await_cycles();
+        top_.set_change_done();
+        top_.t_await_cycles();
+        top_.set_idle();
+      }
+    }
+    TOP & top_;
+    std::size_t n_;
+  };
+  auto task = std::make_unique<VendingMachineFsmTask>(top);
+  TaskRunner.set_task(std::move(task));
+  TaskRunner.run_until_exhausted(true);
 }
 
+int sc_main(int argc, char ** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  ::tb::initialize(argc, argv);
+  return RUN_ALL_TESTS();
+}
