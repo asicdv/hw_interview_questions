871986c01296167fd1eda9a58c7466f7fe68ecab
Stephen Henry <stephenry@gmail.com>
2019-06-06 12:24:24 +0100

    Ported clk_div_by_3 solution.

--- solutions/clocking/clk_div_by_3/clk_div_by_3.cpp
+++ solutions/clocking/clk_div_by_3/clk_div_by_3.cpp
@@ -25,95 +25,130 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //========================================================================== //
 
-#include <libtb2.hpp>
+#include "libtb/libtb.hpp"
+#include "libtb/verilator.hpp"
+#include <gtest/gtest.h>
+#include <queue>
+#include <cmath>
 #include "vobj/Vclk_div_by_3.h"
 
 #define PORTS(__func)                           \
   __func(clk_div_3, bool)
-typedef Vclk_div_by_3 uut_t;
 
-struct ClkDivBy3Tb : libtb2::Top<ClkDivBy3Tb> {
-  SC_HAS_PROCESS(ClkDivBy3Tb);
-  ClkDivBy3Tb(sc_core::sc_module_name mn = "t")
-      : uut_("uut")
-#define __construct_signals(__name, __type)     \
-      , __name##_(#__name)
-      PORTS(__construct_signals)
-#undef __construct_signals
-  {
-    //
-    resetter_.clk(clk_);
-    resetter_.rst(rst_);
-    //
-    wd_.clk(clk_);
-    sampler_.clk(clk_);
-    //
-    uut_.clk(clk_);
-    uut_.rst(rst_);
+struct TOP : tb::Top {
+  TOP() {
+    v.rst(rst);
+    v.clk(clk);
 #define __bind_signals(__name, __type)          \
-    uut_.__name(__name##_);
+    v.__name(__name);
     PORTS(__bind_signals)
 #undef __bind_signals
-
-    SC_METHOD(m_clk1);
-    sensitive << clk_.posedge_event()
-              << rst_.posedge_event();
-    dont_initialize();
-    
-    SC_METHOD(m_clk2);
-    sensitive << clk_div_3_.posedge_event()
-              << rst_.posedge_event();
-    dont_initialize();
-
-    SC_METHOD(m_checker);
-    sensitive << sampler_.sample();
-    dont_initialize();
-
-    count_1_ = 0;
-    count_2_ = 0;
+    start_tracing();
   }
- private:
-  void m_clk1() {
-    if (rst_)
-      count_1_ = 0;
-    else
-      count_1_++;
+  ~TOP() {
+    stop_tracing();
   }
-  void m_clk2() {
-    if (rst_)
-      count_2_ = 0;
-    else
-      count_2_++;
+  void t_apply_reset() {
+    rst = true;
+    t_await_cycles(2);
+    rst = false;
+    t_await_cycles(2);
   }
-  void m_checker() {
-    if (count_1_ == 0)
-      return ;
-
-    if (count_1_ % 33 == 0) {
-      LIBTB2_ERROR_ON(count_2_ % 11 != 0);
-
-      LOGGER(INFO) << " Fast = " << count_1_ << " Slow = " << count_2_
-                   << " Ratio = " << (count_1_ / count_2_)
-                   << "\n";
-    }
+  void t_await_cycles(std::size_t n = 1) {
+    while (n--)
+      wait(clk.negedge_event());
   }
-  
-  std::size_t count_1_, count_2_;
-
-  libtb2::Resetter resetter_;
-  libtb2::SimWatchDogCycles wd_;
-  libtb2::Sampler sampler_;
-  sc_core::sc_clock clk_;
-  sc_core::sc_signal<bool> rst_;
-#define __declare_signals(__name, __type)       \
-  sc_core::sc_signal<__type> __name##_;
-  PORTS(__declare_signals)
-#undef __declare_signals
-  uut_t uut_;
+  void t_await_cycles_slow(std::size_t n = 1) {
+    while (n--)
+      wait(clk_div_3.negedge_event());
+  }
+  Vclk_div_by_3 v{"clk_div_by_3"};
+  ::sc_core::sc_clock clk{"clk"};
+  ::sc_core::sc_signal<bool> rst{"rst"};
+#define __declare_signal(__name, __type)        \
+  ::sc_core::sc_signal<__type> __name{#__name};
+  PORTS(__declare_signal)
+#undef __declare_signal
+  private:
+  void start_tracing() {
+#ifdef OPT_ENABLE_TRACE
+    Verilated::traceEverOn(true);
+    vcd_ = std::make_unique<VerilatedVcdSc>();
+    v.trace(vcd_.get(), 99);
+    vcd_->open("sim.vcd");
+#endif
+  }
+  void stop_tracing() {
+#ifdef OPT_ENABLE_TRACE
+    vcd_->close();
+#endif
+  }
+#ifdef OPT_ENABLE_TRACE
+  std::unique_ptr<VerilatedVcdSc> vcd_;
+#endif
 };
-SC_MODULE_EXPORT(ClkDivBy3Tb);
 
-int sc_main(int argc, char **argv) {
-  ClkDivBy3Tb tb;
-  return libtb2::Sim::start(argc, argv);
+namespace {
+
+TOP top;
+tb::TaskRunner TaskRunner;
+
+} // namespace
+
+TEST(ClkDivBy3Test, Basic) {
+  struct ClkCounterTask : ::tb::Task {
+    ClkCounterTask(TOP & top) : top_(top) {}
+    void execute() override {
+      {
+        sc_spawn_options opts;
+        opts.set_sensitivity(&top_.clk.negedge_event());
+        opts.spawn_method();
+        opts.dont_initialize();
+
+        sc_process_handle h_check = 
+            sc_spawn(std::bind(&ClkCounterTask::m_fast_clock, this),
+                     "m_fast_clock", &opts );
+      }
+      {
+        sc_spawn_options opts;
+        opts.set_sensitivity(&top_.clk_div_3.negedge_event());
+        opts.spawn_method();
+        opts.dont_initialize();
+
+        sc_process_handle h_check = 
+            sc_spawn(std::bind(&ClkCounterTask::m_slow_clock, this),
+                     "m_slow_clock", &opts );
+      }
+
+      slow_n_ = 0;
+      fast_n_ = 0;
+
+      top_.t_await_cycles(300);
+
+      ASSERT_EQ(fast_n_, 300);
+
+      top_.t_await_cycles_slow();
+      ASSERT_EQ(slow_n_, 100);
+      
+      finish();
+    }
+   private:
+    void m_slow_clock() {
+      ++slow_n_;
+    }
+    void m_fast_clock() {
+      ++fast_n_;
+    }
+    std::size_t fast_n_, slow_n_;
+    TOP & top_;
+  };
+  auto task = std::make_unique<ClkCounterTask>(top);
+  TaskRunner.set_task(std::move(task));
+  TaskRunner.run_until_exhausted(true);
+}
+
+int sc_main(int argc, char ** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  ::tb::initialize(argc, argv);
+  return RUN_ALL_TESTS();
 }
