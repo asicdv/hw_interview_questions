5d1dc825f136411a1a9884ad4b5a3e816c3b88c0
Stephen Henry <stephenry@gmail.com>
2019-06-14 21:35:42 -0700

    Ported linked_list_fifo solution.

--- solutions/queues/linked_list_fifo/linked_list_fifo.cpp
+++ solutions/queues/linked_list_fifo/linked_list_fifo.cpp
@@ -1,5 +1,5 @@
 //========================================================================== //
-// Copyright (c) 2016, Stephen Henry
+// Copyright (c) 2017, Stephen Henry
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -25,216 +25,231 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //========================================================================== //
 
-#include <libtb2.hpp>
+#include "libtb/libtb.hpp"
+#include "libtb/verilator.hpp"
+#include <gtest/gtest.h>
+#include <vector>
 #include <deque>
 #include "vobj/Vlinked_list_fifo.h"
+
+using word_type = uint32_t;
 
 #define PORTS(__func)                           \
   __func(cmd_pass, bool)                        \
   __func(cmd_push, bool)                        \
-  __func(cmd_id, uint32_t)                      \
-  __func(cmd_push_data, uint32_t)               \
-  __func(cmd_pop_data, uint32_t)                \
+  __func(cmd_id, word_type)                     \
+  __func(cmd_push_data, word_type)              \
+  __func(cmd_pop_data, word_type)               \
   __func(cmd_pop_data_vld_r, bool)              \
   __func(clear, bool)                           \
   __func(full_r, bool)                          \
   __func(empty_r, bool)                         \
-  __func(nempty_r, uint32_t)                    \
+  __func(nempty_r, word_type)                   \
   __func(busy_r, bool)
 
-typedef Vlinked_list_fifo uut_t;
-
-const int ID_N = 4;
-const int PTR_N = 255;
-
-struct CmdXActorIntf : sc_core::sc_interface {
-  virtual void idle() = 0;
-  virtual void push(uint32_t id, uint32_t data) = 0;
-  virtual bool is_full() = 0;
-  virtual bool is_empty() = 0;
-  virtual uint32_t pop(uint32_t id) = 0;
-  virtual uint32_t id()  = 0;
-  virtual uint32_t non_empty_id() = 0;
+struct Stimulus {
+  friend std::ostream & operator<<(std::ostream & os, const Stimulus & stim) {
+    return os << "'{id=" << stim.id() << ",data=" << stim.data() << "}";
+  }
+  Stimulus(word_type id, word_type data) : id_(id), data_(data) {}
+  word_type id() const { return id_; }
+  word_type data() const { return data_; }
+ private:
+  word_type id_, data_;
 };
 
-struct CmdXActor : CmdXActorIntf, sc_core::sc_module {
-  //
-  sc_core::sc_in<bool> clk;
-  sc_core::sc_in<bool> rst;
-  //
-  sc_core::sc_out<bool> cmd_pass;
-  sc_core::sc_out<bool> cmd_push;
-  sc_core::sc_out<uint32_t> cmd_id;
-  sc_core::sc_out<uint32_t> cmd_push_data;
-  sc_core::sc_in<uint32_t> cmd_pop_data;
-  sc_core::sc_in<bool> cmd_pop_data_vld_r;
-  sc_core::sc_out<bool> clear;
-  sc_core::sc_in<bool> full_r;
-  sc_core::sc_in<bool> empty_r;
-  sc_core::sc_in<uint32_t> nempty_r;
-  sc_core::sc_in<bool> busy_r;
+struct Expect {
+  friend std::ostream & operator<<(std::ostream & os, const Expect & res) {
+    return os << "'{data=" << res.data() << "}";
+  }
+  friend bool operator==(const Expect & a, const Expect & b) {
+    return a.data() == b.data();
+  }
+  Expect(word_type data) : data_(data) {}
+  word_type data() const { return data_; }
+ private:
+  word_type data_;
+};
 
-  CmdXActor(sc_core::sc_module_name mn = "t")
-    : clk("clk"), rst("rst")
-#define __construct_ports(__name, __type)       \
-    , __name(#__name)
-    PORTS(__construct_ports)
-#undef __construct_ports
-  {
-    sampler_.clk(clk);
+struct TOP : tb::Top {
+  using stimulus_type = Stimulus;
+  using expected_type = Expect;
+
+  TOP() {
+    v.rst(rst);
+    v.clk(clk);
+#define __bind_signals(__name, __type)          \
+    v.__name(__name);
+    PORTS(__bind_signals)
+#undef __bind_signals
+    start_tracing();
+  }
+  ~TOP() {
+    stop_tracing();
   }
   void idle() {
     cmd_pass = false;
-    cmd_push = false;
-    cmd_id = 0;
-    cmd_push_data = 0;
+    cmd_push = word_type{};
+    cmd_id = word_type{};
+    cmd_push_data = word_type{};
   }
-  bool is_full() {
-    sampler_.wait_for_sample();
-    return full_r;
-  }
-  uint32_t non_empty_id() {
-    sampler_.wait_for_sample();
-    return libtb2::pickone(libtb2::mask_bits(nempty_r.read(), ID_N)) - 1;
-  }
-  uint32_t id() {
-    struct inrange_constraint : scv_constraint_base {
-      scv_smart_ptr<uint32_t> p;
-      SCV_CONSTRAINT_CTOR(inrange_constraint) {
-        SCV_CONSTRAINT((p() >= 0) && (p() < ID_N)); 
-      }
-    };
-    static inrange_constraint c("inrange_constraint");
-    c.next();
-    return *c.p;
-  }
-  bool is_empty() {
-    sampler_.wait_for_sample();
-    return empty_r;
-  }
-  void push(uint32_t id, uint32_t data) {
-    LIBTB2_ERROR_ON(full_r);
+  void t_push(const stimulus_type & stim) {
+    t_wait_not_full();
     cmd_pass = true;
     cmd_push = true;
-    cmd_id = id;
-    cmd_push_data = data;
-    wait(clk.posedge_event());
-    LOGGER(DEBUG) << "Push ID = " << id << " Push Data = " << data << "\n";
-    q_[id].push_back(data);
+    cmd_id = stim.id();
+    cmd_push_data = stim.data();
+    t_await_cycles(1);
     idle();
   }
-  uint32_t pop(uint32_t id) {
-    LIBTB2_ERROR_ON(q_[id].empty());
+  Expect t_pop(word_type vq) {
+    t_wait_not_nempty(vq);
     cmd_pass = true;
     cmd_push = false;
-    cmd_id = id;
-    wait(clk.posedge_event());
+    cmd_id = vq;
+    t_await_cycles(1);
     idle();
-
-    do {
-      sampler_.wait_for_sample();
-    } while (!cmd_pop_data_vld_r);
-
-    LOGGER(DEBUG) << "Pop ID = " << id << " Pop Data = " << cmd_pop_data << "\n";
-    const uint32_t expected = q_[id].front(); q_[id].pop_front();
-    const uint32_t actual = cmd_pop_data;
-    LIBTB2_ERROR_ON(expected != actual);
-    return actual;
+    return Expect{cmd_pop_data};
   }
-private:
-  std::deque<uint32_t> q_[ID_N];
-  libtb2::Sampler sampler_;
-};
-
-enum OP { NOP, PUSH, POP };
-template<>
-struct scv_extensions<OP> : public scv_enum_base<OP> {
-  SCV_ENUM_CTOR(OP) {
-    SCV_ENUM(NOP);
-    SCV_ENUM(PUSH);
-    SCV_ENUM(POP);
+  void t_apply_reset() {
+    rst = true;
+    t_await_cycles(2);
+    rst = false;
+    t_await_cycles(2);
   }
-};
-
-struct LinkedListFifoTb : libtb2::Top<LinkedListFifoTb> {
-  typedef Vlinked_list_fifo uut_t;
-  sc_core::sc_port<CmdXActorIntf> xact_;
-  SC_HAS_PROCESS(LinkedListFifoTb);
-  LinkedListFifoTb(sc_core::sc_module_name mn = "t")
-    : uut_("uut") {
-    //
-    resetter_.clk(clk_);
-    resetter_.rst(rst_);
-    //
-    sampler_.clk(clk_);
-    //
-    wd_.clk(clk_);
-    //
-    xact_.bind(x_);
-    //
-    x_.clk(clk_);
-    x_.rst(rst_);
-#define __bind_ports(__name, __type)            \
-    x_.__name(__name ## _);
-    PORTS(__bind_ports)
-#undef __bind_ports
-    //
-    uut_.clk(clk_);
-    uut_.rst(rst_);
-#define __bind_ports(__name, __type)            \
-    uut_.__name(__name ## _);
-    PORTS(__bind_ports)
-#undef __bind_ports
-    SC_THREAD(t_stimulus);
+  void t_wait_not_full() {
+    while (full_r)
+      t_await_cycles();
   }
-private:
-  
-  void t_stimulus() {
-    scv_smart_ptr<uint32_t> ppush_data;
-    
-    xact_->idle();
-    resetter_.wait_reset_done();
-    while (true) {
-      scv_bag<OP> op;
-      op.add(NOP, 10);
-      if (!xact_->is_full())
-        op.add(PUSH, 40);
-      if (!xact_->is_empty())
-        op.add(POP, 40);
-
-      scv_smart_ptr<OP> iop;
-      iop->set_mode(op);
-      iop->next();
-      switch (*iop) {
-      case NOP: {
-        wait(clk_.posedge_event());
-      } break;
-      case PUSH: {
-        ppush_data->next();
-        xact_->push(xact_->id(), *ppush_data);
-      } break;
-      case POP: {
-        xact_->pop(xact_->non_empty_id());
-      } break;
-
-      }
-    }
+  void t_wait_not_empty() {
+    while (empty_r)
+      t_await_cycles();
   }
-  CmdXActor x_;
-  sc_core::sc_clock clk_;
-  sc_core::sc_signal<bool> rst_;
+  void t_wait_not_nempty(word_type id) {
+    while (tb::bit(nempty_r, id))
+      t_await_cycles();
+  }
+  void t_await_cycles(std::size_t n = 1) {
+    while (n--)
+      wait(clk.negedge_event());
+  }
+  Vlinked_list_fifo v{"linked_list_fifo"};
+  ::sc_core::sc_signal<bool> rst{"rst"};
+  ::sc_core::sc_clock clk{"clk"};
 #define __declare_signal(__name, __type)        \
-  sc_core::sc_signal<__type> __name##_;
+  ::sc_core::sc_signal<__type> __name{#__name};
   PORTS(__declare_signal)
 #undef __declare_signal
-  libtb2::Resetter resetter_;
-  libtb2::Sampler sampler_;
-  libtb2::SimWatchDogCycles wd_;
-  uut_t uut_;
+ private:
+  void start_tracing() {
+#ifdef OPT_ENABLE_TRACE
+    Verilated::traceEverOn(true);
+    vcd_ = std::make_unique<VerilatedVcdSc>();
+    v.trace(vcd_.get(), 99);
+    vcd_->open("sim.vcd");
+#endif
+  }
+  void stop_tracing() {
+#ifdef OPT_ENABLE_TRACE
+    vcd_->close();
+#endif
+  }
+#ifdef OPT_ENABLE_TRACE
+  std::unique_ptr<VerilatedVcdSc> vcd_;
+#endif
 };
-SC_MODULE_EXPORT(LinkedListFifoTb);
 
-int sc_main(int argc, char **argv) {
-  LinkedListFifoTb tb;
-  return libtb2::Sim::start(argc, argv);
+namespace {
+
+TOP top;
+tb::TaskRunner TaskRunner;
+
+} // namespace
+
+TEST(MultiplierTest, Basic) {
+  const std::size_t n{1024 << 6};
+  struct FifoNTask : tb::Task {
+    FifoNTask(TOP & top) : top_(top) {
+      expect_.resize(4);
+      
+      bgbool.add(false, 10);
+      bgbool.add(true, 10);
+      bgbool.finalize();
+    }
+    void add_stimulus(const Stimulus & s) {
+      stimulus_.push_back(s);
+    }
+    void execute() override {
+      using namespace sc_core;
+
+      top_.t_apply_reset();
+      
+      sc_process_handle h_pusher = 
+        sc_spawn(std::bind(&FifoNTask::t_command_issue, this), "t_command_issue");
+      sc_process_handle h_popper = 
+        sc_spawn(std::bind(&FifoNTask::t_data_collector, this), "t_data_collector");
+    }
+   private:
+    void t_command_issue() {
+      while (!stimulus_.empty()) {
+        top_.idle();
+        if (bgbool()) {
+          const Stimulus & stim{stimulus_.front()};
+          top_.t_push(stim);
+          expect_[stim.id()].push_back(Expect{stim.data()});
+          stimulus_.pop_front();
+        } else {
+          top_.t_await_cycles();
+        }
+      }
+      top_.idle();
+      finish();
+    }
+    void t_data_collector() {
+      tb::Random::Bag<word_type> bg;
+      std::vector<word_type> available_id;
+
+      bool done{false};
+      while (!done) {
+        get_outstanding(available_id);
+        if (available_id.empty()) {
+          done = stimulus_.empty();
+          if (!done)
+            top_.t_await_cycles(10);
+        } else {
+          bg.add(available_id.begin(), available_id.end());
+          const word_type id{bg()};
+          const Expect rtl{top_.t_pop(id)};
+          const Expect tb{expect_[id].front()};
+          ASSERT_EQ(rtl, tb);
+          expect_[id].pop_front();
+          bg.clear();
+        }
+      }
+    }
+    void get_outstanding(std::vector<word_type> & v) const {
+      v.clear();
+      for (std::size_t q = 0; q < expect_.size(); q++) {
+        if (!expect_[q].empty())
+          v.push_back(q);
+      }
+    }
+    std::vector<std::deque<Expect> > expect_;
+    std::deque<Stimulus> stimulus_;
+    tb::Random::Bag<bool> bgbool;
+    TOP & top_;
+  };
+  
+  auto task = std::make_unique<FifoNTask>(top);
+    tb::Random::UniformRandomInterval<word_type> rnd_vq{3,0}, rnd_data{};
+  for (std::size_t i = 0; i < n; i++)
+    task->add_stimulus(Stimulus{rnd_vq(), rnd_data()});
+  TaskRunner.set_task(std::move(task));
+  TaskRunner.run_until_exhausted(true);
+}
+
+int sc_main(int argc, char ** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  ::tb::initialize(argc, argv);
+  return RUN_ALL_TESTS();
 }
