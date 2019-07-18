e910746205d7c42d2f39c07634d4932ad9ce5a73
Stephen Henry <stephenry@gmail.com>
2019-07-17 23:34:12 -0700

    Delete build files committed by mistake.

--- b/solutions/arithmetic/simd_shifter/CMakeFiles/verilate_tb_simd_shifter.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/verilate_tb_simd_shifter"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/verilate_tb_simd_shifter.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
